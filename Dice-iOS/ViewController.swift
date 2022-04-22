7ca70bdcb37c83b6d5c37f0bb8ed260058aee7b9
Alan Nugraha <yaelahlan@gmail.com>
2022-04-23 04:58:12 +0700

    first init app with Swift

--- Dice-iOS/ViewController.swift
+++ Dice-iOS/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceSix")
     }
 
-
+    @IBAction func diceButtonPressed(_ sender: UIButton) {
+        
+        let diceImageArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        
+        diceImageViewOne.image = diceImageArray.randomElement()
+        diceImageViewTwo.image = diceImageArray.randomElement()
+    }
+    
 }
 
